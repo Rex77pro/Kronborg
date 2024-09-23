da2b4f625d6280aaa4c720799fda20ed975a66a5
Rex77pro <smaurieka@gmail.com>
2024-09-23 14:33:44 +0200

    homepage - start

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ function App() {
       <header>
       </header>
       <main>
+        <div style={{ 
+          backgroundImage: 'url(/bgPic.jpg)', /* Referer til billedet direkte */ 
+          height: '800px', /* Sæt en højde for at se baggrundsbilledet */ 
+          backgroundSize: 'cover', /* Dæk hele området */ 
+          textAlign: 'center',
+          textAnchor: 'end',
+          color: 'white' }}
+        >
+          <h1>Rasmus Kronborg Ahlstrand</h1>
+        </div>
       </main>
       <footer>
       </footer>

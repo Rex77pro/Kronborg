6459e28525079bff2bb296b64db10fcce56b4134
Rex77pro <smaurieka@gmail.com>
2024-10-13 21:34:38 +0200

    style(page): Fill skillPage with relevant information

--- src/Pages/SkillsPage.js
+++ src/Pages/SkillsPage.js
@@ -3,8 +3,40 @@ import React from "react";
 const SkillPage = () => {
     return (
         <div>
-            
-        </div>
+            <div className='container pt-7' style={{ textAlign: 'center'}}>
+                <div class="row" >
+                    <div class='col'>
+                        <h5 className='fw-bold'>Frontend</h5>
+                        <br></br>
+                        <p>HTML</p>
+                        <p>CSS</p>
+                        <p>Bootstrap</p>
+                        <p>React</p>
+                        <p>Vue</p>
+                        <p>TypeScript</p>
+                    </div>
+                    <div class='col'>
+                    <h5 className='fw-bold'>Backend</h5>
+                        <br></br>
+                        <p>Node.js</p>
+                        <p>PHP</p>
+                        <p>C#</p>
+                        <p>Python</p>
+                        <p>Kotlin</p>
+                        <p>.Net</p>
+                    </div>
+                    <div class='col'>
+                    <h5 className='fw-bold'>Api & Databases</h5>
+                        <br></br>
+                        <p>MySQL</p>
+                        <p>MsSQL</p>
+                        <p>SQL</p>
+                        <p>REST</p>
+                        <p>GraphQL</p>
+                    </div>
+                </div>
+            </div>
+        </div>  
     );
 };
 

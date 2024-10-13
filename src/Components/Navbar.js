b3a64a12a10b145c8ad807c4152d17831286e986
Rex77pro <smaurieka@gmail.com>
2024-10-14 00:19:12 +0200

    fix(nav): Create uniqe css to include "//"
    Create css file to include "//" before my routes in the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import './Navbar.css' 
 
 const MyNavbar = () => {
     const location = useLocation();
@@ -21,8 +22,8 @@ const MyNavbar = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">   
                 <Nav className ='ms-auto'>
-                    <Nav.Link href='/'>Home</Nav.Link>
-                    <Nav.Link href='/skills'>Skills</Nav.Link>
+                    <Nav.Link href='/' className="uniqe-link">Home</Nav.Link>
+                    <Nav.Link href='/skills' className="uniqe-link">Skills</Nav.Link>
                 </Nav> 
             </Navbar.Collapse>
         </Navbar>
